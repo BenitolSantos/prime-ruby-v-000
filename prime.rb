49f0920b94a57ae5bfe087bca8eab85d95631719
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-03-29 15:26:31 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -20,7 +20,8 @@ def factorial(integer)
   factorialed_num
 end
 
-#puts prime?(105557)
+
+
 
 #A prime number anything divisible by itself alone and 1/ not including 1 and 0
 #what is n! ? n! is (n,n-1....0)
